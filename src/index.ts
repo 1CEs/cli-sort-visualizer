673d30b39e51d2489ff31feff7ee61fceaa320a7
1CEs <isongwut.me@gmail.com>
2025-05-03 16:01:04 +0700

    update: call utilities"

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { generateRandomArray } from "./utils/randomize";
+import { Sort } from "./utils/sort";
 
 
-(() => {
-    console.log("Hello")
+(async () => {
+    const data = generateRandomArray(Math.floor(Math.random() * 25))
+    const sort = new Sort(data)
+    await sort.bubble()
 })();
